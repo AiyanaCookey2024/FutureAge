2342c9fd49f35bc5f9d0dd8785189b57db81fcde
AiyanaCookey2024 <167078825+AiyanaCookey2024@users.noreply.github.com>
2024-04-15 14:17:53 +0000

    initial setup

---
+++ age.js
@@ -0,0 +1,8 @@
+const readline = require("readline-sync");
+let userAge = readline.question("Please enter your age:")
+let currentAge = 10;
+let currentYear = 2024;
+let futureYear = 2028;
+
+console.log (userAge)
+
