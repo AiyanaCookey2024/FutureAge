35acc333f8a2cd55d513d9630d4a6d93de73ec08
AiyanaCookey2024 <167078825+AiyanaCookey2024@users.noreply.github.com>
2024-04-15 15:11:07 +0000

    new script

--- age.js
+++ age.js
@@ -1,8 +1,11 @@
 const readline = require("readline-sync");
 let userAge = readline.question("Please enter your age:")
-let currentAge = 10;
 let currentYear = 2024;
 let futureYear = 2028;
+let userAgeNum = parseInt(userAge);
+let futureAge = (futureYear - currentYear) + userAgeNum;  // 4 + "21"
+console.log(`${futureAge}`);
 
-console.log (userAge)
+
+    //" hello ${var} yyy"
 
