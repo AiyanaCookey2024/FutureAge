0f6dfd531c6b40adc2247c8fcbd1e89c8f72030a
AiyanaCookey2024 <167078825+AiyanaCookey2024@users.noreply.github.com>
2024-04-16 11:30:12 +0000

    .

--- task3.js
+++ task3.js
@@ -1,6 +1,8 @@
 console.log("task3")
 
 //Use a loop to print the numbers from 10 to 1 (in descending order)
-for( i = 1, i<= 10, i++){
-    console.log(11- i)
+for( i = 10; i> 0; i--) {
+    console.log(i)
 }
+// -- is shorthand for writting i-1
+
---
+++ task4.js
@@ -0,0 +1,7 @@
+console.log("task4")
+
+//Use a loop to print the numbers between 1 and 1000 that are divisible by 3 and 4.
+
+for(let i = 1; i <=1000; i++){
+    console.log(i)
+}
