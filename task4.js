fed7619bd77f4939f425bd148721e7e93df1d1d5
AiyanaCookey2024 <167078825+AiyanaCookey2024@users.noreply.github.com>
2024-04-16 11:47:09 +0000

    .

--- task4.js
+++ task4.js
@@ -2,6 +2,14 @@ console.log("task4")
 
 //Use a loop to print the numbers between 1 and 1000 that are divisible by 3 and 4.
 
-for(let i = 1; i <=1000; i++){
-    console.log(i)
+for(let i = 1; i <=15; i++){
+    let calc = i%3; //use the modulus - when the modulus is 0 we know that the remainder is 0 which means that it is a whole number
+    if (calc == 0) {
+        console.log(`${i} divisible by 3 = ${calc}`)
+    }
+   
+    calc = i%4;
+    if (calc == 0) {
+        console.log(`${i} divisible by 4 = ${calc}`)
+    }
 }
