35b5dc42aee454257938f14ce890a50d78301a37
AiyanaCookey2024 <167078825+AiyanaCookey2024@users.noreply.github.com>
2024-04-16 08:23:41 +0000

    .

--- challenge1.js
+++ challenge1.js
@@ -1,10 +1,10 @@
 //** Ask the user to enter their name and their favourite number. The computer will then greet the user by name and their favourite number is the same as the computer, it will output a message saying "that's my favourite number as well".  
 const readline = require("readline-sync");
-let computerfav = 7;
+let favNum = 7;
 let username = readline.question("What is your name?");
 let userNum = readline.question("What is your favourite number?");
 console.log(`Hello ${username}.`);
 
-if computerfav == userNum {
+if (favNum == userNum) {
     console.log(`That's my favourite number as well`);
 }
---
+++ dogyears.js
@@ -0,0 +1,6 @@
+const readline = require("readline-sync");
+let dogAge = readline.question("Please enter your dog's age:")
+let dogAge = 2
+if (dogAge < 2) {
+    console.log(``)
+}
