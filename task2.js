463d3e9d2cc18d242d601be79d26ff8eb897be95
AiyanaCookey2024 <167078825+AiyanaCookey2024@users.noreply.github.com>
2024-04-16 11:22:35 +0000

    commit

--- task2.js
+++ task2.js
@@ -1,4 +1,10 @@
 console.log("Task2")
 
-for(let i = 1; i <=100; i++)
+//Use a loop to print out the squares for the numbers between 1 and 100.
+
+for(let i = 1; i <=100; i++){
+    //this is where I loop
+    console.log(i**2) // this will show the power to 2 - where the power function is ** or use console.log(Math.pow(i,2))
+
+}
 
---
+++ task3.js
@@ -0,0 +1,6 @@
+console.log("task3")
+
+//Use a loop to print the numbers from 10 to 1 (in descending order)
+for( i = 1, i<= 10, i++){
+    console.log(11- i)
+}
