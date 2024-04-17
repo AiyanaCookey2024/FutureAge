ae41a3f705271f7531e793c89e2829faec796953
AiyanaCookey2024 <167078825+AiyanaCookey2024@users.noreply.github.com>
2024-04-17 16:23:50 +0000

    even odd notes

--- notes.js
+++ notes.js
@@ -1,3 +1,11 @@
-let j = 10
-var k = 10
+//let j = 10
+//var k = 10
 // using var k, thsi will exist permanantely, this can be reassigned outside this. If we used let we can use it for teh whole file - let is a more control version of var
+function eventChecker(number) {
+    if (number % 2 == 0) {
+        return "even"
+    }
+    else {
+        return "odd"
+    }
+}
