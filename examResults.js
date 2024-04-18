1d0575525cb63d56a1fd9f62eb21de59b28ce807
AiyanaCookey2024 <167078825+AiyanaCookey2024@users.noreply.github.com>
2024-04-18 09:31:48 +0000

    .

---
+++ examResults.js
@@ -0,0 +1,6 @@
+const examResults = {
+    steve: [99, 98, 95],
+    violet: [56, 53, 88],
+    aqil: [100, 100, 100]
+} 
+console.log(examResults.steve.includes("hello"))
--- notes.js
+++ notes.js
@@ -1,11 +1,6 @@
 //let j = 10
 //var k = 10
 // using var k, thsi will exist permanantely, this can be reassigned outside this. If we used let we can use it for teh whole file - let is a more control version of var
-function eventChecker(number) {
-    if (number % 2 == 0) {
-        return "even"
-    }
-    else {
-        return "odd"
-    }
+function myFunction() {
+    return Math.PI;
 }
